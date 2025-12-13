f652c6b180d5684bad6429379443c476321a8875
Dhruv Sheth <dhruvsheth@Dhruvs-MacBook-Air.local>
2025-12-13 21:58:39 +0530

    Fix Express wildcard route for Render

--- backend/server.js
+++ backend/server.js
@@ -56,10 +56,12 @@ app.use(express.urlencoded({ extended: true }));
 // });
 app.use("/api/v1", infoAuthRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "dist", "index.html")
+  );
 });
 
 if (cluster.isPrimary) {
